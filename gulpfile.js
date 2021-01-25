0f4bbb7e6b255d42e5807f46fda4c1b6c091e721
Miroslav Krýsl <mkrysl@protonmail.com>
2021-01-25 14:42:46 +0100

    Init flask project

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+let gulp = require('gulp');
+let clean = require('gulp-clean');
+let merge = require('merge-stream');
+let sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+gulp.task('compile:css', function () {
+    return gulp.src('resources/css/**/*.sass')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('static/css'));
+});
+
+
+gulp.task('compile:js', function () {
+    return gulp.src('resources/js/**/*.js')
+            .pipe(gulp.dest('static/js'));
+});
+
+gulp.task('compile:images', function () {
+    return gulp.src('resources/images/**/*')
+            .pipe(gulp.dest('static/images'));
+});
+
+gulp.task('libs', function () {
+    return merge(
+        gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css'),
+        gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css.map'),
+        gulp.src('node_modules/bootstrap/dist/js/bootstrap.bundle.js'),
+        gulp.src('node_modules/jquery/dist/jquery.min.js'),
+        gulp.src('node_modules/sockjs-client/dist/sockjs.min.js'),
+    ).pipe(gulp.dest('static/lib'));
+});
+
+gulp.task('compile', gulp.parallel('compile:css', 'compile:js', 'compile:images'));
+
+gulp.task('clean', function () {
+    return gulp.src([
+        'static/**/*'
+    ], {read: false}).pipe(clean())
+});
+
+gulp.task('bundle', gulp.series('clean', gulp.parallel('compile', 'libs')));
