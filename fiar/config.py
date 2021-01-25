852989c988895d8e98b2506fdeb4825efdf16a16
Miroslav Krýsl <mkrysl@protonmail.com>
2021-01-25 17:26:10 +0100

    Fix config

--- fiar/config.py
+++ fiar/config.py
@@ -3,16 +3,23 @@ from abc import ABC
 
 class Config(ABC):
     SECRET_KEY = 'super secret key'
-    DB_DRIVER = 'postgresql'
-    DB_HOST = 'localhost'
-    DB_PORT = '5432'
-    DB_NAME = 'pia'
-    DB_USER = 'pia'
-    DB_PASSWORD = 'pia'
+
+    APP_DB_DRIVER = 'postgresql'
+    APP_DB_HOST = 'localhost'
+    APP_DB_PORT = '5432'
+    APP_DB_NAME = 'pia'
+    APP_DB_USER = 'pia'
+    APP_DB_PASSWORD = 'pia'
+
+    # --- SQLAlchemy ---
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @property
-    def DB_URI(self):
-        return f'{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+    def SQLALCHEMY_DATABASE_URI(self):
+        return f'{self.APP_DB_DRIVER}://' \
+               f'{self.APP_DB_USER}:{self.APP_DB_PASSWORD}' \
+               f'@{self.APP_DB_HOST}:{self.APP_DB_PORT}' \
+               f'/{self.APP_DB_NAME}'
 
 
 class ProductionConfig(Config):
