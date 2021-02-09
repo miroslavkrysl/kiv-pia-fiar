a3a61c269a403ffc361bad7354534317c19f9c4a
Miroslav Krýsl <mkrysl@protonmail.com>
2021-02-09 10:06:50 +0100

    Finalize db schema

--- fiar/db.py
+++ fiar/db.py
@@ -1,155 +1,73 @@
 from datetime import datetime
+from enum import IntEnum
 
-from flask import Flask, g
 from pony.orm import *
 
 database = Database()
+
+
+class Player(IntEnum):
+    O = 0
+    X = 1
 
 
 class User(database.Entity):
     id = PrimaryKey(int, auto=True)
     username = Required(str, 32, unique=True)
     email = Required(str, 255, unique=True)
-    password = Required(str, 100)
+    password = Required(str, 60)
     is_admin = Required(bool)
     uid = Required(str, 128, unique=True)
     last_active_at = Required(datetime)
+    last_playing_at = Required(datetime)
+
+    sent_friendship_requests = Set('FriendshipRequest', reverse='sender')
+    received_friendship_requests = Set('FriendshipRequest', reverse='recipient')
+    sent_game_invites = Set('GameInvite', reverse='sender')
+    received_game_invites = Set('GameInvite', reverse='recipient')
+    sent_friendships = Set('Friendship', reverse='sender')
+    received_friendships = Set('Friendship', reverse='recipient')
+    games_as_o = Set('Game', reverse='player_o')
+    games_as_x = Set('Game', reverse='player_x')
 
 
-# import enum
-# from datetime import datetime
-#
-# from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, Enum, UniqueConstraint, DateTime, inspect
-# from sqlalchemy.ext.declarative import declarative_base, as_declarative
-#
-#
-# @as_declarative()
-# class Base:
-#     def to_dict(self):
-#         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
-#
-#
-# class User(Base):
-#     __tablename__ = 'user'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     username = Column(String(32), unique=True)
-#     email = Column(String(254), unique=True)
-#     password = Column(String(255), nullable=False)
-#     is_admin = Column(Boolean, default=False, nullable=False)
-#     last_active_at = Column(DateTime, default=datetime.now, nullable=False)
-#     active_game_id = Column(Integer, ForeignKey('game.id', use_alter=True, name='fk_user_active_game_id'), default=None, nullable=True)
-#     uid = Column(String(64), nullable=False, unique=True)
-
-# active_game = relationship("Game", foreign_keys="User.active_game_id")
+class Game(database.Entity):
+    id = PrimaryKey(int, auto=True)
+    moves = Set('Move')
+    player_o = Required(User, reverse='games_as_o')
+    player_x = Required(User, reverse='games_as_x')
+    winner = Optional(int)
 
 
-# class Friendship(Base):
-#     __tablename__ = 'friendship'
-#     from_user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-#     to_user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-#
-#     # from_user = relationship("User", foreign_keys="Friendship.from_user_id")
-#     # to_user = relationship("User", foreign_keys="Friendship.to_user_id")
-#
-#
-# class FriendshipRequest(Base):
-#     __tablename__ = 'friendship_request'
-#     from_user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-#     to_user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-#
-#     # from_user = relationship("User", foreign_keys="FriendshipRequest.from_user_id")
-#     # to_user = relationship("User", foreign_keys="FriendshipRequest.to_user_id")
-#
-#
-# class Invitation(Base):
-#     __tablename__ = 'invitation'
-#     from_user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-#     to_user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-#
-#     # from_user = relationship("User", foreign_keys="Invitation.from_user_id")
-#     # to_user = relationship("User", foreign_keys="Invitation.to_user_id")
-#
-#
-# class Player(enum.Enum):
-#     O = 0
-#     X = 1
-#
-#
-# class Game(Base):
-#     __tablename__ = 'game'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     player_o_id = Column(Integer, ForeignKey('user.id', use_alter=True, name='fk_game_player_o_id'), nullable=False)
-#     player_x_id = Column(Integer, ForeignKey('user.id', use_alter=True, name='fk_game_player_x_id'), nullable=False)
-#
-#     # player_o = relationship("User", foreign_keys=[player_o_id])
-#     # player_x = relationship("User", foreign_keys=[player_x_id])
-#     # result = relationship('GameResult', uselist=False)
-#
-#     __table_args__ = (
-#         UniqueConstraint('id', 'player_o_id', 'player_x_id'),
-#     )
-#
-#
-# class Move(Base):
-#     __tablename__ = 'move'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     player = Column(Enum(Player), nullable=False)
-#     pos_x = Column(Integer, nullable=False)
-#     pos_y = Column(Integer, nullable=False)
-#
-#     __table_args__ = (
-#         UniqueConstraint('player', 'pos_x', 'pos_y'),
-#     )
-#
-#
-# class GamesMoves(Base):
-#     __tablename__ = 'games_moves'
-#     game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
-#     move_id = Column(Integer, ForeignKey('move.id'), primary_key=True)
-#
-#     game = relationship('Game')
-#     move = relationship('Move')
-#
-#     __table_args__ = (
-#         UniqueConstraint('game_id', 'move_id'),
-#     )
-#
-#
-# class GameResult(Base):
-#     __tablename__ = 'game_result'
-#     game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
-#     winner_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-#
-#     game = relationship('Game', uselist=False)
-#     winner = relationship('User', uselist=False)
+class Move(database.Entity):
+    id = PrimaryKey(int, auto=True)
+    player = Required(int)
+    x = Required(int)
+    y = Required(int)
+    games = Set(Game)
+
+    composite_key(player, x, y)
 
 
-class Db:
-    G_PREFIX = 'db_'
-    G_SESSION = G_PREFIX + 'session'
+class FriendshipRequest(database.Entity):
+    id = PrimaryKey(int, auto=True)
+    sender = Required(User, reverse='sent_friendship_requests')
+    recipient = Required(User, reverse='received_friendship_requests')
 
-    def __init__(self, app: Flask):
-        db_config = app.config['DATABASE']
-        database.bind(provider=db_config['PROVIDER'],
-                      user=db_config['USER'],
-                      password=db_config['PASSWORD'],
-                      host=db_config['HOST'],
-                      database=db_config['NAME'])
-        database.generate_mapping(check_tables=False)
+    composite_key(sender, recipient)
 
-        self.database = database
 
-        app.before_request(self.enter_session)
-        app.teardown_appcontext(lambda e: self.exit_session(e))
+class GameInvite(database.Entity):
+    id = PrimaryKey(int, auto=True)
+    sender = Required(User, reverse='sent_game_invites')
+    recipient = Required(User, reverse='received_game_invites')
 
-    def enter_session(self):
-        if not hasattr(g, self.G_SESSION):
-            session = db_session()
-            session.__enter__()
-            setattr(g, self.G_SESSION, session)
+    composite_key(sender, recipient)
 
-    def exit_session(self, exc=None):
-        if hasattr(g, self.G_SESSION):
-            session = getattr(g, self.G_SESSION)
-            session.__exit__(exc=exc)
-            delattr(g, self.G_SESSION)
+
+class Friendship(database.Entity):
+    id = PrimaryKey(int, auto=True)
+    sender = Required(User, reverse='sent_friendships')
+    recipient = Required(User, reverse='received_friendships')
+
+    composite_key(sender, recipient)
